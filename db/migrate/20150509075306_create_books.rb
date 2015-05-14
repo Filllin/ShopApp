950a6102733ab5d89bbf778822236a7b18c1182b
Fillin <kozeyandrey@gmail.com>
2015-05-14 22:40:58 +0300

    Update migrate file - create_books.rb

--- db/migrate/20150509075306_create_books.rb
+++ db/migrate/20150509075306_create_books.rb
@@ -5,17 +5,20 @@ class CreateBooks < ActiveRecord::Migration
       t.string :description
       t.integer :reference
       t.integer :ISBN
-      t.string :author
-      t.string :publisher
+      t.string  :author
+      t.string  :publisher
       t.integer :number_of_pages
-      t.string :format
+      t.string  :format
       t.integer :weight
       t.integer :height
       t.integer :width
       t.integer :thickness
-      t.string :category
+      t.belongs_to  :sub_category, index: true
+      t.string  :slug
+
 
       t.timestamps null: false
     end
+    add_index :books, :slug, :unique => true
   end
 end
