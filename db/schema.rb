58f09c1dc481803f5fff49281a6ea30f8f234744
Fillin <kozeyandrey@gmail.com>
2015-05-14 22:42:19 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508200615) do
+ActiveRecord::Schema.define(version: 20150510124348) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -26,15 +26,47 @@ ActiveRecord::Schema.define(version: 20150508200615) do
     t.integer  "height"
     t.integer  "width"
     t.integer  "thickness"
-    t.string   "category"
+    t.integer  "sub_category_id"
+    t.string   "slug"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
 
+  add_index "books", ["slug"], name: "index_books_on_slug", unique: true
+  add_index "books", ["sub_category_id"], name: "index_books_on_sub_category_id"
+
   create_table "categories", force: :cascade do |t|
-    t.string   "book"
+    t.string   "title"
+    t.string   "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "categories", ["slug"], name: "index_categories_on_slug", unique: true
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "sub_categories", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "category_id"
+    t.string   "slug"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "sub_categories", ["category_id"], name: "index_sub_categories_on_category_id"
+  add_index "sub_categories", ["slug"], name: "index_sub_categories_on_slug", unique: true
 
 end
