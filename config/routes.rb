4137be09f76e38388020d172a23eff5d9ea711cb
Fillin <kozeyandrey@gmail.com>
2015-05-16 22:54:58 +0300

    Add link on payment page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
   get 'contacts' => "default#contacts", as: :contacts
 
   get 'about' => "default#about", as: :about
+
+  get 'oplata-i-dostavka' => "default#payment", as: :payment
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
