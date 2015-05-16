40166829de9db05b5254edb33bfb4ef290b6a65d
Fillin <kozeyandrey@gmail.com>
2015-05-16 21:30:33 +0300

    Add route on contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'contacts' => "default#contacts", as: :contacts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
