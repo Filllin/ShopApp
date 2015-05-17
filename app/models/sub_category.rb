e60f1f1e880b7b8feb786dd2b02af6e7721526aa
Fillin <kozeyandrey@gmail.com>
2015-05-17 22:50:19 +0300

    Update sub_category.rb

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,5 +1,6 @@
 class SubCategory < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
-  has_many :products
+  has_many :products, dependent: :destroy
+  belongs_to :category
 end
