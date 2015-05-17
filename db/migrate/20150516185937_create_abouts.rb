297945f3a4878a9e888f6f786795b5ca97a26842
Fillin <kozeyandrey@gmail.com>
2015-05-17 22:33:17 +0300

    Update in 20150516185937_create_abouts.rb

--- db/migrate/20150516185937_create_abouts.rb
+++ db/migrate/20150516185937_create_abouts.rb
@@ -2,10 +2,6 @@ class CreateAbouts < ActiveRecord::Migration
   def change
     create_table :abouts do |t|
       t.text :content
-      t.string :email
-      t.text :google_maps_code
-      t.text :schedule
-      t.text :location
 
       t.timestamps null: false
     end
