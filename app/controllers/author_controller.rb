88267e75d9382fc9754da8c7516db9544bad8b17
Fillin <kozeyandrey@gmail.com>
2015-05-18 20:39:54 +0300

    Create author_controller.rb

---
+++ app/controllers/author_controller.rb
@@ -0,0 +1,23 @@
+class AuthorController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
+  def view_author
+    if params[:count].present?
+      @products = Product.where(author: Author.where(slug: params[:slug])).order(sort_column + " " + sort_direction).paginate(:per_page => params[:count], :page => params[:page])
+      respond_to do |format|
+        format.html { redirect_to view_author_path, notice: "Вам будет показано #{params[:count]} товаров" }
+      end
+    else
+      @products = Product.where(author: Author.where(slug: params[:slug])).order(sort_column + " " + sort_direction).paginate(:per_page => 10, :page => params[:page])
+    end
+    @categories = Category.all
+  end
+
+  def sort_column
+    Product.column_names.include?(params[:sort]) ? params[:sort] : "title"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+end
