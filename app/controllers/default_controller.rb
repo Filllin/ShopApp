34a6eecf76da655b5f1e4c2f3b324d1691bcddae
Fillin <kozeyandrey@gmail.com>
2015-05-18 19:30:52 +0300

    Update default_controller.rb

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -32,22 +32,4 @@ class DefaultController < ApplicationController
       end
     end
   end
-
-  def sub_category_view
-    if params[:count].present?
-      @products = Product.where(sub_category: SubCategory.where(slug: params[:slug])).order(sort_column + " " + sort_direction).paginate(:per_page => params[:count], :page => params[:page])
-    else
-      @products = Product.where(sub_category: SubCategory.where(slug: params[:slug])).order(sort_column + " " + sort_direction).paginate(:per_page => 10, :page => params[:page])
-    end
-    @categories = Category.all
-  end
-
-  def sort_column
-    Product.column_names.include?(params[:sort]) ? params[:sort] : "title"
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
-
 end
