388851783600278cae10808339c21befc4e8b74a
Fillin <kozeyandrey@gmail.com>
2015-05-18 19:30:22 +0300

    Create category_controller.rb

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,30 @@
+class CategoryController < ApplicationControlle
+  def category_view
+    if params[:slug].present?
+      @slug_category = Category.where(slug: params[:slug])
+      if params[:count].present?
+        @products = Product.where(sub_category: SubCategory.where(category: @slug_category)).order(sort_column + " " + sort_direction).paginate(:per_page => params[:count], :page => params[:page])
+      else
+        @products = Product.where(sub_category: SubCategory.where(category: @slug_category)).order(sort_column + " " + sort_direction).paginate(:per_page => 10, :page => params[:page])
+      end
+    end
+    @categories = Category.all
+  end
+
+  def sub_category_view
+    if params[:count].present?
+      @products = Product.where(sub_category: SubCategory.where(slug: params[:slug])).order(sort_column + " " + sort_direction).paginate(:per_page => params[:count], :page => params[:page])
+    else
+      @products = Product.where(sub_category: SubCategory.where(slug: params[:slug])).order(sort_column + " " + sort_direction).paginate(:per_page => 10, :page => params[:page])
+    end
+    @categories = Category.all
+  end
+
+  def sort_column
+    Product.column_names.include?(params[:sort]) ? params[:sort] : "title"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+end
