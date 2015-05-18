d2a863c75a000e7102a7ee8aa6960cca66e13510
Fillin <kozeyandrey@gmail.com>
2015-05-18 14:23:08 +0300

    Create method category_view

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -28,4 +28,16 @@ class DefaultController < ApplicationController
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
+
+  def category_view
+    if params[:slug].present?
+      @slug_category = Category.where(slug: params[:slug])
+      if params[:count].present?
+        @products = Product.where(sub_category: SubCategory.where(category: @slug_category)).order(sort_column + " " + sort_direction).paginate(:per_page => params[:count], :page => params[:page])
+      else
+        @products = Product.where(sub_category: SubCategory.where(category: @slug_category)).order(sort_column + " " + sort_direction).paginate(:per_page => 10, :page => params[:page])
+      end
+    end
+    @categories = Category.all
+  end
 end
