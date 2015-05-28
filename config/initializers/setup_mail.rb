22c1f6c29280120212825b011d635663f0c809a6
Fillin <kozeyandrey@gmail.com>
2015-05-28 18:57:43 +0300

    Update  parameters: port and domain in setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     :address              => "friendsbottom.gmail.com",
-    :port                 => 25,
-    :domain               => "gmail.com",
+    :port                 => 587,
+    :domain               => "heroku.com",
     :user_name            => "example@friendsbote.herokuapp.com",
     :password             => ENV['SMTP_PASSWORD'],
     :authentication       => "plain",
