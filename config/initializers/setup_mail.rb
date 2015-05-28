48468d4a0363d471e928aa672c9858cabcd2911a
Fillin <kozeyandrey@gmail.com>
2015-05-28 19:31:55 +0300

    Update  parameters  in setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.sendgrid.net",
+    :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "heroku.com",
-    :user_name            => "example@friendsbote.herokuapp.com",
+    :domain               => "gmail.com",
+    :user_name            => "kozeyandrey@gmail.com",
     :password             => ENV['SMTP_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
