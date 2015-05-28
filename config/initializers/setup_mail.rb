8696442efad93b6a8faa9ec2778ce77ee55dff41
Fillin <kozeyandrey@gmail.com>
2015-05-28 18:34:49 +0300

    Update config/initializers/setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
+    :address              => "friendsbottom.gmail.com",
+    :port                 => 25,
     :domain               => "gmail.com",
-    :user_name            => "example@n3wnormal.com",
-    :password             => "passwordforemail",
+    :user_name            => "example@friendsbote.herokuapp.com",
+    :password             => ENV['SMTP_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
 }
