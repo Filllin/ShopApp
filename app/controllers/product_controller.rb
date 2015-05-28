84e745abe025a33d25177f7cbcccf9afee0392a8
Fillin <kozeyandrey@gmail.com>
2015-05-28 16:09:11 +0300

    Updare product_controller.rb

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -10,10 +10,16 @@ class ProductController < ApplicationController
       else
           CustomerProduct.create(user_session_id: session['session_id'], product: @product, quantity: params[:count_of_products])
       end
-      @quantity_products = @product.quantity_products - params[:count_of_products].to_i
-      @product.update(quantity_products: @quantity_products)
-      respond_to do |format|
-        format.html { redirect_to view_product_path, notice: "Товар #{@title} добавлен в корзину" }
+      if @product.quantity_products >= params[:count_of_products].to_i
+        @quantity_products = @product.quantity_products - params[:count_of_products].to_i
+        @product.update(quantity_products: @quantity_products)
+        respond_to do |format|
+          format.html { redirect_to view_product_path, notice: "Товар #{@title} добавлен в корзину" }
+        end
+      else
+        respond_to do |format|
+          format.html { redirect_to view_product_path, notice: "Простите, вы не можите купить этот товар количеством в #{params[:count_of_products].to_i} так как их всего #{@product.quantity_products}" }
+        end
       end
     end
   end
