54e6d49bda5568f2ac436b10114a1d04395c055c
Fillin <kozeyandrey@gmail.com>
2015-05-29 23:12:10 +0300

    Create method destroy_product in models/customer_product.rb

--- app/models/customer_product.rb
+++ app/models/customer_product.rb
@@ -1,4 +1,11 @@
 class CustomerProduct < ActiveRecord::Base
     belongs_to :product
     belongs_to :customer
+
+    def self.destroy_product(product_title, quantity_of_products, session)
+      product = Product.find_by_title(product_title)
+      CustomerProduct.where(user_session_id: session, product: product, customer: nil).take.destroy
+      quantity_products = product.quantity_products + quantity_of_products.to_i
+      product.update(quantity_products: quantity_products)
+    end
 end
