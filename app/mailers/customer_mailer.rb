d977138097a75afd4fb15e62588ecf74cd0a5735
Fillin <kozeyandrey@gmail.com>
2015-05-29 09:12:58 +0300

    Update mailers/customer_mailer.rb

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,7 +1,7 @@
 class CustomerMailer < ActionMailer::Base
   default from: 'friendsboten@gmail.com'
 
-  def welcome_email(user,customers_product)
+  def customer_email(user,customers_product)
     @user = user
     @customers_product = customers_product
     mail(to: @user.email, subject: 'Заказ на FriendsBote')
