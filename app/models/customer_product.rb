9ffee7b360d91bb2a11cefcc4b5bfe0ee893605c
Fillin <kozeyandrey@gmail.com>
2015-05-29 23:30:51 +0300

    Create method update_quantity_product

--- app/models/customer_product.rb
+++ app/models/customer_product.rb
@@ -8,4 +8,16 @@ class CustomerProduct < ActiveRecord::Base
       quantity_products = product.quantity_products + quantity_of_products.to_i
       product.update(quantity_products: quantity_products)
     end
+
+    def self.update_quantity_product(product_title, quantity_of_products, session)
+      product = Product.find_by_title(product_title)
+      customer_product = CustomerProduct.where(user_session_id: session, product: product, customer: nil).first
+      if quantity_of_products.to_i < customer_product.quantity
+        quantity_products = product.quantity_products + (customer_product.quantity - quantity_of_products.to_i)
+      else
+        quantity_products = product.quantity_products - (quantity_of_products.to_i - customer_product.quantity)
+      end
+      product.update(quantity_products: quantity_products)
+      customer_product.update(quantity: quantity_of_products)
+    end
 end
