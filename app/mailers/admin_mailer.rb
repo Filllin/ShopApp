08877630c14c727a998afac8b9a732eb561324cb
Fillin <kozeyandrey@gmail.com>
2015-05-29 17:13:07 +0300

    Remove mailers/admin_mailer.rb

--- app/mailers/admin_mailer.rb
+++
@@ -1,7 +0,0 @@
-class AdminMailer < ActionMailer::Base
-
-  def admin_email(user)
-    @user = user
-    mail(to: 'kozeyandrey@gmail.com', subject: 'Данные о заказе и заказчика с FriendsBote')
-  end
-end
