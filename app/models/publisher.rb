b06efe118f5f6c655214f09536a9b0b21044e74e
Fillin <kozeyandrey@gmail.com>
2015-05-29 23:44:47 +0300

    Create method count_products in models/publisher.rb

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -2,4 +2,8 @@ class Publisher < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
   has_many :products
+
+  def self.count_products(count, slug, page, sort_column, sort_direction)
+    return Product.where(publisher: Publisher.where(slug: slug)).order(sort_column + " " + sort_direction).paginate(:per_page => count, :page => page)
+  end
 end
