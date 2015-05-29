30b9d02e6338ac087aa768ac4f577726437ddf25
Fillin <kozeyandrey@gmail.com>
2015-05-29 20:15:16 +0300

    Update models/product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,8 @@ class Product < ActiveRecord::Base
   def self.search(search)
     where("(LOWER(title) LIKE :search OR LOWER(description) LIKE :search)", search: "%#{search}%")
   end
+
+  def self.count_products_by_category(slug_category, count, page, sort_column, sort_direction)
+    return Product.where(sub_category: SubCategory.where(category: slug_category)).order(sort_column + " " + sort_direction).paginate(:per_page => count, :page => page)
+  end
 end
