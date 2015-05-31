ba730577a2f283add02af1a31383f3056ad393a0
Fillin <kozeyandrey@gmail.com>
2015-05-31 23:59:48 +0300

    Update models/product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ class Product < ActiveRecord::Base
   belongs_to :author
   has_many :customer_products
   has_many :customers, through: :customer_products
+  mount_uploader :image, ImageUploader
 
   def self.search(search)
     where("(LOWER(title) LIKE :search OR LOWER(description) LIKE :search)", search: "%#{search}%")
