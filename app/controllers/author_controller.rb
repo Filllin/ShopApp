008b508e21236f5a9864724861ad8394728a1f38
Fillin <kozeyandrey@gmail.com>
2015-06-03 13:55:07 +0300

    Create description methods in author_controller.rb

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,6 +1,7 @@
 class AuthorController < ApplicationController
   helper_method :sort_column, :sort_direction
 
+  # Return the products by author
   def view_author
     @categories = Category.all
     if params[:count].present?
@@ -9,13 +10,5 @@ class AuthorController < ApplicationController
     else
       @products = Author.count_products(10, params[:slug], params[:page], sort_column, sort_direction)
     end
-  end
-
-  def sort_column
-    Product.column_names.include?(params[:sort]) ? params[:sort] : "title"
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
