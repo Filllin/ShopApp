9e9946bdaa60f043f72b9f277741f79ac490ae44
Fillin <kozeyandrey@gmail.com>
2015-06-03 13:55:52 +0300

    Create description methods in default_controller.rb

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,4 +1,6 @@
 class DefaultController < ApplicationController
+
+  # Return search page
   def search
     @categories = Category.all
     @search = params[:search].mb_chars.downcase.to_s
@@ -7,22 +9,26 @@ class DefaultController < ApplicationController
     end
   end
 
+  # Return home page
   def home
     @products = Product.all
     @categories = Category.all
     @count = 20
   end
 
+  # Return contacts page
   def contacts
     @categories = Category.all
     @contacts = Contact.first
   end
 
+  # Return about page
   def about
     @about = About.first
     @categories = Category.all
   end
 
+  # Return payment page
   def payment
     @title = Payment.first.title
     @payment = Payment.first
