1acbfef820f3f5b4a1b806f4d1d18e8b73e52cd8
Fillin <kozeyandrey@gmail.com>
2015-06-03 14:06:12 +0300

    Create description by method in models/customer_product.rb

--- app/models/customer_product.rb
+++ app/models/customer_product.rb
@@ -2,6 +2,7 @@ class CustomerProduct < ActiveRecord::Base
     belongs_to :product
     belongs_to :customer
 
+    # Destroy object CustomerProduct by product and update quantity products
     def self.destroy_product(product_title, quantity_of_products, session)
       product = Product.find_by_title(product_title)
       CustomerProduct.where(user_session_id: session, product: product, customer: nil).take.destroy
@@ -9,6 +10,7 @@ class CustomerProduct < ActiveRecord::Base
       product.update(quantity_products: quantity_products)
     end
 
+    # Update quantity products
     def self.update_quantity_product(customer_product, product, quantity_of_products)
       if quantity_of_products.to_i < customer_product.quantity
         quantity_products = product.quantity_products + (customer_product.quantity - quantity_of_products.to_i)
