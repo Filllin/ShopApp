bff49ea7261f35896b3aa03056ec632a353b2e00
Fillin <kozeyandrey@gmail.com>
2015-06-03 13:56:12 +0300

    Create description methods in publisher_controller.rb

--- app/controllers/publisher_controller.rb
+++ app/controllers/publisher_controller.rb
@@ -1,6 +1,7 @@
 class PublisherController < ApplicationController
   helper_method :sort_column, :sort_direction
 
+  # Return products by publisher
   def view_publisher
     @categories = Category.all
     if params[:count].present?
@@ -9,13 +10,5 @@ class PublisherController < ApplicationController
     else
       @products = Publisher.count_products(10, params[:slug], params[:page], sort_column, sort_direction)
     end
-  end
-
-  def sort_column
-    Product.column_names.include?(params[:sort]) ? params[:sort] : "title"
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
