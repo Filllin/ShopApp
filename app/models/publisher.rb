722080a8f535927880abc2b9062fe4aebb272580
Fillin <kozeyandrey@gmail.com>
2015-06-03 14:06:35 +0300

    Create description by method in models/publisher.rb

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -3,6 +3,7 @@ class Publisher < ActiveRecord::Base
   friendly_id :title, use: :slugged
   has_many :products
 
+  # Return products by publisher
   def self.count_products(count, slug, page, sort_column, sort_direction)
     return Product.where(publisher: Publisher.where(slug: slug)).order(sort_column + " " + sort_direction).paginate(:per_page => count, :page => page)
   end
