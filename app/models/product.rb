0d3ead02c4eb8e4526f8968db5e299a64a684004
Fillin <kozeyandrey@gmail.com>
2015-06-03 14:06:24 +0300

    Create description by method in models/product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,24 +8,29 @@ class Product < ActiveRecord::Base
   has_many :customers, through: :customer_products
   mount_uploader :image, ImageUploader
 
+  # Return search result
   def self.search(search)
     where("(LOWER(title) LIKE :search OR LOWER(description) LIKE :search)", search: "%#{search}%")
   end
 
+  # Return products by category
   def self.count_products_by_category(slug_category, count, page, sort_column, sort_direction)
     return Product.where(sub_category: SubCategory.where(category: slug_category)).order(sort_column + " " + sort_direction).paginate(:per_page => count, :page => page)
   end
 
+  # Return products by sub_category
   def self.count_products_by_sub_category(slug, count, page, sort_column, sort_direction)
     return Product.where(sub_category: SubCategory.find_by_slug(slug)).order(sort_column + " " + sort_direction).paginate(:per_page => count, :page => page)
   end
 
+  # Add object Product to Cart
   def self.add_product_to_cart(product, count, session)
     quantity_products = product.quantity_products - count.to_i
     product.update(quantity_products: quantity_products)
     Product.create_customer(product, count, session)
   end
 
+  # Create object CustomerProduct
   def self.create_customer(product, count, session)
     customer_product = CustomerProduct.where(user_session_id: session, product: product, customer: nil).take
     if customer_product.present?
