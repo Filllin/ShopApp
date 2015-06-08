ff2b591c54a19bcee0550a741cf279c8b22d1c17
Fillin <kozeyandrey@gmail.com>
2015-06-08 22:34:39 +0300

    Update send_email.rb

--- app/mailers/send_mailer.rb
+++ app/mailers/send_mailer.rb
@@ -1,15 +1,17 @@
 class SendMailer < ActionMailer::Base
   default from: 'friendsboten@gmail.com'
 
-  def customer_email(user,customers_product)
+  def customer_email(user,customers_product, coupon)
     @user = user
     @customers_product = customers_product
+    @coupon = coupon
     mail(to: @user.email, subject: 'Заказ на FriendsBote')
   end
 
-  def admin_email(user, customers_product)
+  def admin_email(user, customers_product, coupon)
     @user = user
     @customers_product = customers_product
+    @coupon = coupon
     mail(to: 'dix.alex@gmail.com', subject: 'Данные о заказе и заказчика с FriendsBote')
   end
 end
