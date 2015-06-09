945574c75934f56184a7bba8db6de3031cde8409
Fillin <kozeyandrey@gmail.com>
2015-06-09 11:03:47 +0300

    Fixed work with flash in product_controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -7,9 +7,9 @@ class ProductController < ApplicationController
     if params[:count_of_products].present?
       if @product.quantity_products >= params[:count_of_products].to_i
        Product.add_product_to_cart(@product, params[:count_of_products], session['session_id'])
-        flash[:notice] = "Товар #{ title} добавлен в корзину"
+        flash.now[:notice] = "Товар #{ title} добавлен в корзину"
       else
-        flash[:notice] = "Простите, вы не можите купить этот товар количеством в #{params[:count_of_products].to_i} так как их всего #{@product.quantity_products}"
+        flash.now[:notice] = "Простите, вы не можите купить этот товар количеством в #{params[:count_of_products].to_i} так как их всего #{@product.quantity_products}"
       end
     end
   end
