6424926afb4251487571191a1ef7a26352ca9262
Fillin <kozeyandrey@gmail.com>
2015-06-09 11:03:56 +0300

    Fixed work with flash in publisher_controller

--- app/controllers/publisher_controller.rb
+++ app/controllers/publisher_controller.rb
@@ -6,7 +6,7 @@ class PublisherController < ApplicationController
     @categories = Category.all
     if params[:count].present?
       @products = Publisher.count_products(params[:count], params[:slug], params[:page], sort_column, sort_direction)
-      flash[:notice] = "Вам будет показано #{params[:count]} товаров"
+      flash.now[:notice] = "Вам будет показано #{params[:count]} товаров"
     else
       @products = Publisher.count_products(10, params[:slug], params[:page], sort_column, sort_direction)
     end
