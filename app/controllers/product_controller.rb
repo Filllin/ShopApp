5c40bd1473c2af4c6a6a8084c3c613c07cd0842e
Fillin <kozeyandrey@gmail.com>
2015-06-10 15:25:34 +0300

    Update product_controller.rb

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -3,11 +3,11 @@ class ProductController < ApplicationController
   # Return product by slug and add product to cart
   def view_product
     @product = Product.find_by_slug(params[:slug])
-    title = @product.title
+    @title = @product.title
     if params[:count_of_products].present?
       if @product.quantity_products >= params[:count_of_products].to_i
        Product.add_product_to_cart(@product, params[:count_of_products], session['session_id'])
-        flash.now[:notice] = "Товар #{ title} добавлен в корзину"
+        flash.now[:notice] = "Товар #{@title} добавлен в корзину"
       else
         flash.now[:notice] = "Простите, вы не можите купить этот товар количеством в #{params[:count_of_products].to_i} так как их всего #{@product.quantity_products}"
       end
