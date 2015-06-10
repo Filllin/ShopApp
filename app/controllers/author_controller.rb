ad7ee76b5872a522b1a0ced76c41d0e94b04262b
Fillin <kozeyandrey@gmail.com>
2015-06-10 15:23:56 +0300

    Update author_controller.rb

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -4,11 +4,12 @@ class AuthorController < ApplicationController
   # Return the products by author
   def view_author
     @categories = Category.all
+    @author = Author.find_by_slug(params[:slug])
     if params[:count].present?
-      @products = Author.count_products(params[:count], params[:slug], params[:page], sort_column, sort_direction)
+      @products = Author.count_products(params[:count], @author, params[:page], sort_column, sort_direction)
       flash.now[:notice] = "Вам будет показано #{params[:count]} товаров"
     else
-      @products = Author.count_products(10, params[:slug], params[:page], sort_column, sort_direction)
+      @products = Author.count_products(10, @author, params[:page], sort_column, sort_direction)
     end
   end
 end
