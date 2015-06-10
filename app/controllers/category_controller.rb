17e857519e40f1eaa5c304141e6578256772d092
Fillin <kozeyandrey@gmail.com>
2015-06-10 15:27:00 +0300

    Update category_controller.rb

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -4,21 +4,22 @@ class CategoryController < ApplicationController
   # Return products by category
   def view_category
     @categories = Category.all
-    slug_category = Category.where(slug: params[:slug])
+    @category = Category.find_by_slug(params[:slug])
     if params[:count].present?
-      @products = Product.count_products_by_category(slug_category, params[:count], params[:page], sort_column, sort_direction)
+      @products = Product.count_products_by_category(@category, params[:count], params[:page], sort_column, sort_direction)
     else
-      @products = Product.count_products_by_category(slug_category, 10, params[:page], sort_column, sort_direction)
+      @products = Product.count_products_by_category(@category, 10, params[:page], sort_column, sort_direction)
     end
   end
 
   # Return products by sub_category
   def view_sub_category
+    @sub_category = SubCategory.find_by_slug(params[:slug])
     @categories = Category.all
     if params[:count].present?
-      @products = Product.count_products_by_sub_category(params[:slug], params[:count], params[:page], sort_column, sort_direction)
+      @products = Product.count_products_by_sub_category(@sub_category, params[:count], params[:page], sort_column, sort_direction)
     else
-      @products = Product.count_products_by_sub_category(params[:slug], 10, params[:page], sort_column, sort_direction)
+      @products = Product.count_products_by_sub_category(@sub_category, 10, params[:page], sort_column, sort_direction)
     end
   end
 end
