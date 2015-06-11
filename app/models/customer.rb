b071c9c2ec48ebd769508e86903a4b99a69a84d3
Fillin <kozeyandrey@gmail.com>
2015-06-11 12:41:28 +0300

    Huge update models/customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,13 +2,13 @@ class Customer < ActiveRecord::Base
   has_many :orders
   has_many :products, through: :orders
 
-  validates :name, presence: { message: 'не может быть пустым' }
-  validates :surname, presence: { message: 'не может быть пустым' }
-  validates :phone_number, presence: { message: 'не может быть пустым' }, format: { with: /\d{3}-\d{3}-\d{4}/, message: "не соответствует правильному формату" }
-  validates :country, presence: { message: 'не может быть пустым' }
-  validates :first_address, presence: { message: 'не может быть пустым' }
-  validates :city, presence: { message: 'не может быть пустым' }
-  validates :postcode, presence: { message: 'не может быть пустым' }
-  validates :email, presence: { message: 'не может быть пустым' }, confirmation: { message: 'не соответствует полю "E-mail адрес"' }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "не соответствует правильному формату" }
-  validates :email_confirmation, presence: { message: 'не может быть пустым' }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "не соответствует правильному формату" }
+  validates :name, field: true
+  validates :surname, field: true
+  validates :phone_number, field: true, phone_numer: true
+  validates :country, field: true
+  validates :first_address, field: true
+  validates :city, field: true
+  validates :postcode, field: true
+  validates :email, field: true, confirmation: { message: 'не соответствует полю "E-mail адрес"' }, email: true
+  validates :email_confirmation, field: true, email: true
 end
