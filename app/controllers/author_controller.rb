a286d5b7bba9e9707002f5f217bc1cff54cbe399
Fillin <kozeyandrey@gmail.com>
2015-06-11 12:46:51 +0300

    Update author_controller.rb

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,9 +1,9 @@
 class AuthorController < ApplicationController
+  before_action :categories_variable
   helper_method :sort_column, :sort_direction
 
   # Return the products by author
   def view_author
-    @categories = Category.all
     @author = Author.find_by_slug(params[:slug])
     if params[:count].present?
       @products = Author.count_products(params[:count], @author, params[:page], sort_column, sort_direction)
