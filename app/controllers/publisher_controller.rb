212ad44dfa3aebf0faed6a43e45aabd78f77432b
Fillin <kozeyandrey@gmail.com>
2015-06-12 15:52:25 +0300

    Update check data in publisher_controller

--- app/controllers/publisher_controller.rb
+++ app/controllers/publisher_controller.rb
@@ -5,6 +5,7 @@ class PublisherController < ApplicationController
   # Return products by publisher
   def view_publisher
     @publisher = Publisher.find_by_slug(params[:slug])
+    not_found(@publisher)
     if params[:count].present?
       @products = Publisher.count_products(params[:count], @publisher, params[:page], sort_column, sort_direction)
       flash.now[:notice] = "Вам будет показано #{params[:count]} товаров"
