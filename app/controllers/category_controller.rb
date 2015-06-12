01a1ee676a6360f40a744ec30b999ef719049fe2
Fillin <kozeyandrey@gmail.com>
2015-06-12 15:51:44 +0300

    Update check data in category_controller

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -5,6 +5,7 @@ class CategoryController < ApplicationController
   # Return products by category
   def view_category
     @category = Category.find_by_slug(params[:slug])
+    not_found(@category)
     if params[:count].present?
       @products = Product.count_products_by_category(@category, params[:count], params[:page], sort_column, sort_direction)
     else
@@ -15,6 +16,7 @@ class CategoryController < ApplicationController
   # Return products by sub_category
   def view_sub_category
     @sub_category = SubCategory.find_by_slug(params[:slug])
+    not_found(@sub_category)
     if params[:count].present?
       @products = Product.count_products_by_sub_category(@sub_category, params[:count], params[:page], sort_column, sort_direction)
     else
