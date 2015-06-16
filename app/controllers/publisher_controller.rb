a2686b2a12e4b796d91640dcf7d9610de8cdd6d4
Fillin <kozeyandrey@gmail.com>
2015-06-16 22:02:37 +0300

    Add method search to :before_action  in  publisher_controller

--- app/controllers/publisher_controller.rb
+++ app/controllers/publisher_controller.rb
@@ -1,5 +1,5 @@
 class PublisherController < ApplicationController
-  before_action :categories_variable
+  before_action :categories_variable, :search
   helper_method :sort_column, :sort_direction
 
   # Return products by publisher
