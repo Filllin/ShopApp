978a69d47103096e8694570556a041122ec138bd
Fillin <kozeyandrey@gmail.com>
2015-06-18 11:37:59 +0300

    Huge update models/category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
-  has_many :sub_categories, dependent: :destroy
+  has_many :subcategories, :class_name => "Category", :foreign_key => "parent_category_id"
+  belongs_to :parent_category, :class_name => "Category"
 end
