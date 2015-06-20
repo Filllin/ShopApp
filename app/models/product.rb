8bd481032bd144548a1e39c4dd87402b2804c0a0
Fillin <kozeyandrey@gmail.com>
2015-06-20 09:56:01 +0300

    Huge update models/product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,31 +4,31 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :publisher
   belongs_to :author
-  monetize :price
+  monetize :price_cents
   mount_uploader :image, ImageUploader
 
   # Return products by category
-  def self.count_products_by_category(slug_category, count, page, sort_column, sort_direction)
-    return Product.where(category: slug_category)
-                  .order(sort_column + " " + sort_direction)
-                  .paginate(:per_page => count, :page => page)
+  def self.count_products_by_category(category, count, page, sort_column, sort_direction)
+    self.where(category: category)
+        .order(sort_column + " " + sort_direction)
+        .paginate(:per_page => count, :page => page)
   end
 
   # Add object Product to Cart
-  def self.add_product_to_cart(product, count, session)
-    quantity_products = product.quantity_products - count.to_i
-    product.update(quantity_products: quantity_products)
-    Product.create_customer(product, count, session)
+  def add_product_to_cart(count, session)
+    quantity_products = self.quantity_products - count.to_i
+    self.update(quantity_products: quantity_products)
+    self.create_order_item(count, session)
   end
 
   # Create object Order
-  def self.create_customer(product, count, session)
-    order = Order.find_by(user_session_id: session, product: product, customer: nil)
-    if order.present?
-      quantity = count.to_i + order.quantity
-      order.update(quantity: quantity)
+  def create_order_item(count, session)
+    order = Order.find_by_user_session_id(session) || Order.create(user_session_id: session, status: 'Received')
+    order_item = OrderItem.find_by(product: self, order: order)
+    if order_item.present?
+      order_item.update_quantity_product(count)
     else
-      Order.create(user_session_id: session, product: product, quantity: count)
+      OrderItem.create(order: order, product: self, quantity: count)
     end
   end
 end
