965f3fcab4d3078d592ce63fbde9abd2930377d5
Fillin <kozeyandrey@gmail.com>
2015-06-20 09:49:32 +0300

    Create mapping on Order model in models/customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,5 @@
 class Customer < ActiveRecord::Base
-  has_many :order_items
-  has_many :orders, through: :order_items
+  has_many :orders
 
   validates :name, field: true
   validates :surname, field: true
